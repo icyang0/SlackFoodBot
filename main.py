44344856d5de5e9877b50c1dbc14f319ae97c8d3
icyang0 <icyang0@gmail.com>
2018-10-14 04:34:18 -0700

    minor bug fix. BTW this version is for bad button

--- main.py
+++ main.py
@@ -17,17 +17,18 @@ def handler(event, context):
     todaysDay = str(datetime.now() -  timedelta(hours=7))
     
     #strip out all the other stuff and only save the day
-    todaysDay = todaysDay[8:10]
+    todaysDay = todaysDay[0:10]
     
     #grab the latest info from the table
-    response = dynamodb.get_item(TableName='slackFoodBot', Key={'theKeyIGuess':{'S':'hello'}})
+    response = dynamodb.get_item(TableName='slackFoodBot', Key={'theKeyIGuess':{'S':'dog'}})
     item = response["Item"]
     lastGoodDate =  str(item['goodDate']['S'])
     lastBadDate =  str(item['badDate']['S'])
     goodTotal = int(item['foodGood']['N'])
     badTotal = int(item['foodBad']['N'])
+    keyIndex = item['theKeyIGuess']['S']
     
-    
+ 
     
      #TESTING ONLY DELETE ME    
    # lastGoodDate = 13
@@ -44,11 +45,12 @@ def handler(event, context):
 		
 		
     #check the date of the last known press of the bad button
-    #if the bad button hasnt been pressed today, reset it to 1 (since it's just been pressed)
+    #if the bad button hasnt been pressed today, reset bad button presses to 1 (since it's just been pressed)
     #also update the last known date of button press
-    if todaysDay != lastGoodDate:
+    if todaysDay != lastBadDate:
         badTotal = 1
         lastBadDate = todaysDay
+        
     #if good button has already been pressed today, then add one to the count, but dont need to update the date
     else:
         badTotal = 1 + badTotal		
@@ -58,7 +60,7 @@ def handler(event, context):
    
     
     #now update the table again
-    dynamodb.put_item(TableName='slackFoodBot', Item={'foodGood':{'N':str(goodTotal)},'foodBad':{'N':str(badTotal)}, 'goodDate':{'S':lastGoodDate}, 'badDate':{'S':lastBadDate}, 'theKeyIGuess':{'S':'hello'}})
+    dynamodb.put_item(TableName='slackFoodBot', Item={'foodGood':{'N':str(goodTotal)},'foodBad':{'N':str(badTotal)}, 'goodDate':{'S':lastGoodDate}, 'badDate':{'S':lastBadDate}, 'theKeyIGuess':{'S':'dog'}})
 
     
 
@@ -66,4 +68,4 @@ def handler(event, context):
   #  requests.post('https://hooks.slack.com/services/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',json={"text" : 'Today\'s lunch :heart: : ' + str(goodTotal) })
 	
 	#post to slack for GOOD
-    requests.post('https://hooks.slack.com/services/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',json={"text" : 'Today\'s lunch :nauseated_face: : ' + badTotal })
+    requests.post('https://hooks.slack.com/services/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',json={"text" : 'Today\'s lunch :nauseated_face: : ' + str(badTotal) })
